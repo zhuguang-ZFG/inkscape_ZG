43ef85562bfd2ebcdd85385f0efc8403f0943a5c
zhuguang-ZFG <168892777+zhuguang-ZFG@users.noreply.github.com>
2026-04-28 01:39:28 +0800

    Track GRBL feedback channels separately

--- src/ui/dialog/grbl-panel-feedback-state.h
+++ src/ui/dialog/grbl-panel-feedback-state.h
@@ -9,30 +9,37 @@ namespace Inkscape::UI::Dialog {
 
 struct GrblPlotFeedbackSchedulePlan
 {
-    bool preview_requested = false;
+    bool refresh_summaries_requested = false;
+    bool refresh_overlay_requested = false;
     bool clear_preview_overlay = false;
     bool queue_idle_refresh = false;
 };
 
 struct GrblPlotFeedbackRefreshPlan
 {
-    bool preview_requested = false;
+    bool refresh_summaries_requested = false;
+    bool refresh_overlay_requested = false;
     bool start_timer = false;
 };
 
 struct GrblPlotFeedbackTimerPlan
 {
-    bool preview_requested = false;
+    bool refresh_summaries_requested = false;
+    bool refresh_overlay_requested = false;
     bool refresh_summaries = false;
     bool sync_preview_overlay = false;
 };
 
-GrblPlotFeedbackSchedulePlan make_grbl_plot_feedback_schedule_plan(bool preview_already_requested, bool blocked,
-                                                                  bool refresh_preview, bool dispatch_pending,
-                                                                  bool timer_connected);
-GrblPlotFeedbackRefreshPlan make_grbl_plot_feedback_refresh_plan(bool preview_already_requested, bool blocked,
-                                                                bool refresh_preview, bool timer_connected);
-GrblPlotFeedbackTimerPlan make_grbl_plot_feedback_timer_plan(bool preview_requested, bool preview_enabled);
+GrblPlotFeedbackSchedulePlan make_grbl_plot_feedback_schedule_plan(bool summaries_already_requested,
+                                                                  bool overlay_already_requested, bool blocked,
+                                                                  bool refresh_summaries, bool refresh_overlay,
+                                                                  bool dispatch_pending, bool timer_connected);
+GrblPlotFeedbackRefreshPlan make_grbl_plot_feedback_refresh_plan(bool summaries_already_requested,
+                                                                bool overlay_already_requested, bool blocked,
+                                                                bool refresh_summaries, bool refresh_overlay,
+                                                                bool timer_connected);
+GrblPlotFeedbackTimerPlan make_grbl_plot_feedback_timer_plan(bool refresh_summaries_requested,
+                                                             bool refresh_overlay_requested, bool preview_enabled);
 
 } // namespace Inkscape::UI::Dialog
 
